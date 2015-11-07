405a00c775b9b64f825d11d089fbca233225fc3c
Francis-Groleau <franicsgroleau2@gmail.com>
2015-11-07 00:48:50 -0500

    change surface
    j'ai vu en ligne que c'était mieux d'utilisé un TextureView

--- app/src/main/java/com/example/janicduplessis/myapplication/CameraView.java
+++ app/src/main/java/com/example/janicduplessis/myapplication/CameraView.java
@@ -5,8 +5,11 @@ import android.graphics.Camera;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.TotalCaptureResult;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceHolder;
@@ -19,97 +22,26 @@ import java.util.List;
 /**
  * Created by francis on 2015-11-06.
  */
-public class CameraView extends SurfaceView implements SurfaceHolder.Callback{
+public class CameraView{
 
     private SurfaceHolder _holder;
     private CameraDevice _camera;
     private List<Surface> _surfaces = new ArrayList<Surface>();
     private CameraCaptureSession _currentSession = null;
+    private final static String TAG = "SimpleCamera";
 
-    public CameraView(Context context, CameraDevice camera){
-        super(context);
-
+    public CameraView(Context context, CameraDevice camera, Surface surface){
+        _surfaces.add(surface);
         _camera = camera;
-        //mCamera.setDisplayOrientation(90);
-        //get the holder and set this class as the callback, so we can get camera data here
-        _holder = getHolder();
-        _holder.addCallback(this);
-        _holder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
-
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        try{
-            //when the surface is created, we can set the camera to draw images in this surfaceholder
-            _holder = surfaceHolder;
-            Surface s = surfaceHolder.getSurface();
-            _surfaces.add(s);
 
-            _camera.createCaptureSession(_surfaces, CameraCaptureSessionCallBack, null);
 
-        } catch (CameraAccessException e) {
-            // IllegalArgumentException	if the templateType is not supported by this device.
-            // CameraAccessException	if the camera device is no longer connected or has encountered a fatal error
-            // IllegalStateException	if the camera device has been closed
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
-        //before changing the application orientation, you need to stop the preview, rotate and then start it again
-        if(_holder.getSurface() == null)//check if the surface is ready to receive camera data
-            return;
-        //first stop the preview
-        StopLivePreview();
-
-        //now, recreate the camera preview
-        CaptureRequest request = CreateRequest();
-        if(request != null){
-            SetRequest(request);
-        }
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        //our app has only one screen, so we'll destroy the camera in the surface
-        //if you are unsing with more screens, please move this code your activity
-        StopLivePreview();
-       // mCamera.release();
-    }
-
-    private CaptureRequest CreateRequest(){
+    public void StopLivePreview(){
         try {
-            CaptureRequest.Builder builder = _currentSession.getDevice().createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            builder.addTarget(_surfaces.get(0));
-            CaptureRequest request = builder.build();
-            return request;
-        } catch (CameraAccessException e) {
-            // IllegalArgumentException	if the templateType is not supported by this device.
-            // CameraAccessException	if the camera device is no longer connected or has encountered a fatal error
-            // IllegalStateException	if the camera device has been closed
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private boolean SetRequest (CaptureRequest request){
-        try {
-            _currentSession.setRepeatingRequest(request, CameraCaptureCallback, null);
-            return true;
-        } catch (CameraAccessException e) {
-            // IllegalArgumentException	if the templateType is not supported by this device.
-            // CameraAccessException	if the camera device is no longer connected or has encountered a fatal error
-            // IllegalStateException	if the camera device has been closed
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    private void StopLivePreview(){
-        try {
-            _currentSession.stopRepeating();
+            if(_currentSession != null) {
+                _currentSession.stopRepeating();
+            }
         } catch (CameraAccessException e) {
             e.printStackTrace();
             // IllegalArgumentException	if the templateType is not supported by this device.
@@ -118,23 +50,40 @@ public class CameraView extends SurfaceView implements SurfaceHolder.Callback{
         }
     }
 
-    private final CameraCaptureSession.StateCallback CameraCaptureSessionCallBack = new CameraCaptureSession.StateCallback() {
+
+
+    private CameraCaptureSession.StateCallback CameraCaptureSessionCallBack = new CameraCaptureSession.StateCallback() {
+
         @Override
         public void onConfigured(CameraCaptureSession session) {
-            //the camera is ready to capture !!
+            // TODO Auto-generated method stub
+            Log.i(TAG, "onConfigured");
             _currentSession = session;
-            CaptureRequest request = CreateRequest();
+            CaptureRequest.Builder builder = null;
+            try {
+                builder = _currentSession.getDevice().createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                builder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+                builder.addTarget(_surfaces.get(0));
+                CaptureRequest request = builder.build();
 
-            if(request != null) {
-                SetRequest(request);
+                HandlerThread backgroundThread = new HandlerThread("CameraPreview");
+                backgroundThread.start();
+                Handler backgroundHandler = new Handler(backgroundThread.getLooper());
+
+                _currentSession.setRepeatingRequest(request, CameraCaptureCallback, backgroundHandler);
+
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
             }
         }
 
         @Override
         public void onConfigureFailed(CameraCaptureSession session) {
-            //something went wrong ...
+            // TODO Auto-generated method stub
+            Log.e(TAG, "CameraCaptureSession Configure failed");
         }
     };
+
 
     private final CameraCaptureSession.CaptureCallback CameraCaptureCallback = new CameraCaptureSession.CaptureCallback() {
         @Override
--- app/src/main/java/com/example/janicduplessis/myapplication/MainActivity.java
+++ app/src/main/java/com/example/janicduplessis/myapplication/MainActivity.java
@@ -3,17 +3,27 @@ package com.example.janicduplessis.myapplication;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.app.Activity.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,23 +32,59 @@ public class MainActivity extends AppCompatActivity {
     Context _context = null;
     private boolean _permissionGranted = false;
     private CameraView _cameraView = null;
+    private Size _previewSize = null;
+    private CameraDevice _camera = null;
+
+
+    private final static String TAG = "SimpleCamera";
+    private TextureView mTextureView = null;
+    private TextureView.SurfaceTextureListener mSurfaceTextureListner = new TextureView.SurfaceTextureListener() {
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+            // TODO Auto-generated method stub
+            //Log.i(TAG, "onSurfaceTextureUpdated()");
+
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width,
+                                                int height) {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "onSurfaceTextureSizeChanged()");
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "onSurfaceTextureDestroyed()");
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width,
+                                              int height) {
+            // TODO Auto-generated method stub
+            Log.i(TAG, "onSurfaceTextureAvailable()");
+
+            initializeCamera();
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //same as set-up android:screenOrientation="portrait" in <activity>, AndroidManifest.xml
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
-        _context = this.getBaseContext();
-        initialize();
-
-        //btn to close the application
-        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
-        imgClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.exit(0);
-            }
-        });
+        mTextureView = (TextureView) findViewById(R.id.textureView1);
+        mTextureView.setSurfaceTextureListener(mSurfaceTextureListner);
     }
 
     @Override
@@ -64,19 +110,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
 
-    private void initialize()
+        if (_camera != null)
+        {
+            if(_cameraView != null){
+                _cameraView.StopLivePreview();
+            }
+            _camera.close();
+            _camera = null;
+            _cameraView = null;
+        }
+    }
+
+
+    private void initializeCamera()
     {
+        _context = this.getBaseContext();
         manager = (CameraManager) _context.getSystemService(Context.CAMERA_SERVICE);
         try {
             String[] cameraIdArray = manager.getCameraIdList();
             //find the rear camera
             for (String id : cameraIdArray) {
                 CameraCharacteristics specs = this.manager.getCameraCharacteristics(id);
+
+                StreamConfigurationMap map = specs.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                Size _previewSize = map.getOutputSizes(SurfaceTexture.class)[0];
+
                 int Orientation = specs.get(CameraCharacteristics.LENS_FACING);
                 if (Orientation == CameraCharacteristics.LENS_FACING_BACK) {
-                    if (this.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        this.requestPermissions(new String[]{Manifest.permission.CAMERA}, Constants.REQUEST_CODE_FOR_CAMERA);
+                    try {
+                        ContextCompat compact = new ContextCompat();
+                        if (compact.checkSelfPermission(_context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                            this.requestPermissions(new String[]{Manifest.permission.CAMERA}, Constants.REQUEST_CODE_FOR_CAMERA);
+                        }else{
+                            _permissionGranted = true;
+                        }
+                    }catch(Exception ex){
+                        ex.printStackTrace();
                     }
                     if(_permissionGranted) {
                         manager.openCamera(id, CameraOpenCallBack, null);
@@ -93,9 +167,19 @@ public class MainActivity extends AppCompatActivity {
     private final CameraDevice.StateCallback CameraOpenCallBack = new CameraDevice.StateCallback() {
         @Override
         public void onOpened(CameraDevice camera) {
-            _cameraView = new CameraView(_context, camera);
-            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
-            camera_view.addView(_cameraView);//add the SurfaceView to the layout
+            Log.i(TAG, "onOpened");
+
+            SurfaceTexture texture = mTextureView.getSurfaceTexture();
+            if (texture == null) {
+                Log.e(TAG, "texture is null");
+                return;
+            }
+
+            texture.setDefaultBufferSize(_previewSize.getWidth(), _previewSize.getHeight());
+            Surface surface = new Surface(texture);
+
+
+            _cameraView = new CameraView(_context, camera, surface);
         }
 
         @Override
